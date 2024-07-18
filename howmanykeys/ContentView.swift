97138414dd646a8ae2f2f8974fbe47dda93f2a4f
Jason McGhee <mcghee.j@gmail.com>
2024-07-17 19:15:55 -0700

    Initial Commit

---
+++ howmanykeys/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  howmanykeys
+//
+//  Created by Jason McGhee on 7/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ howmanykeys/howmanykeysApp.swift
@@ -0,0 +1,17 @@
+//
+//  howmanykeysApp.swift
+//  howmanykeys
+//
+//  Created by Jason McGhee on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct howmanykeysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
